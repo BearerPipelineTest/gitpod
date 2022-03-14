15e2c8040c7caf93d1bf46f6a26bd2506ad3f001
Anton Kosyakov <anton@gitpod.io>
2022-03-14 10:30:57 +0000

    build JB images for stable and latest separately

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -5,12 +5,13 @@
 package ide
 
 const (
-	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-93edb90833d8d662f4c1a64b2ad8ced456e62862" // stable version that will be updated manually on demand
-	CodeDesktopIDEImage         = "ide/code-desktop"
-	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
-	IntelliJDesktopIDEImage     = "ide/intellij"
-	GoLandDesktopIdeImage       = "ide/goland"
-	PyCharmDesktopIdeImage      = "ide/pycharm"
-	PhpStormDesktopIdeImage     = "ide/phpstorm"
+	CodeIDEImage                  = "ide/code"
+	CodeIDEImageStableVersion     = "commit-93edb90833d8d662f4c1a64b2ad8ced456e62862" // stable version that will be updated manually on demand
+	CodeDesktopIDEImage           = "ide/code-desktop"
+	CodeDesktopInsidersIDEImage   = "ide/code-desktop-insiders"
+	IntelliJDesktopIDEImage       = "ide/intellij"
+	IntelliJLatestDesktopIDEImage = "ide/intellij-latest"
+	GoLandDesktopIdeImage         = "ide/goland"
+	PyCharmDesktopIdeImage        = "ide/pycharm"
+	PhpStormDesktopIdeImage       = "ide/phpstorm"
 )
